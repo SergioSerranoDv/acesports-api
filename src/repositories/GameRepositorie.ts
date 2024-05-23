e76948222270cd14b898386bd50e5b065f5b57fb
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-23 15:50:15 -0500

    feat: Add method to retrieve game by ID

--- src/repositories/GameRepositorie.ts
+++ src/repositories/GameRepositorie.ts
@@ -2,11 +2,15 @@ import { Game, GameModel } from "../models/Game"
 
 export interface GameRespositorie {
   getAllGames(): Promise<Game[]>
+  getGameById(id: string): Promise<Game | null>
   createGame(game: Game): Promise<Game>
 }
 export class GameMongoRepositorie implements GameRespositorie {
   public async getAllGames(): Promise<Game[]> {
     return GameModel.find()
+  }
+  public async getGameById(id: string): Promise<Game | null> {
+    return GameModel.findById(id)
   }
   public async createGame(game: Game): Promise<Game> {
     const newGame = new GameModel(game)
