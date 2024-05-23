b98bc3679f991ff8810134dcefa9ea444cd34baf
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-23 18:41:19 -0500

    feat: Add route to update tournament

--- src/routes/TournamentRouter.ts
+++ src/routes/TournamentRouter.ts
@@ -11,6 +11,7 @@ export class TournamentRouter {
     this.tournamentController = tournamentController
     this.router = Router()
     this.router.post("/new", this.createTournament)
+    this.router.put("/update/:id", this.updateTournament)
   }
   static getRouter(): Router {
     if (!TournamentRouter.instance) {
@@ -27,7 +28,36 @@ export class TournamentRouter {
       }
       const data = req.body
       const userId = req.user.sub
+      console.log(userId)
       const response = await this.tournamentController.createTournament(data, userId)
+      if (response.status === "error") {
+        return res.status(response.code).send({
+          status: "error",
+          message: response.message,
+        })
+      }
+      return res.status(response.code).send({
+        status: response.status,
+        data: response.data,
+        message: response.message,
+      })
+    } catch (error: any) {
+      return res.status(500).send({
+        status: "error",
+        message: error.message,
+      })
+    }
+  }
+  private updateTournament = async (req: Request, res: Response) => {
+    try {
+      if (!req.user) {
+        return res.status(403).send({
+          message: "unauthorized",
+        })
+      }
+      const tournamentId = req.params.id
+      const data = req.body
+      const response = await this.tournamentController.updateTournament(tournamentId, data)
       if (response.status === "error") {
         return res.status(response.code).send({
           status: "error",
