2acef0095efea4bf5d59a0287d882825a87b0b97
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-23 18:59:36 -0500

    feat: Add route to get tournament by ID

--- src/routes/TournamentRouter.ts
+++ src/routes/TournamentRouter.ts
@@ -11,6 +11,7 @@ export class TournamentRouter {
     this.tournamentController = tournamentController
     this.router = Router()
     this.router.get("/by-user", this.getTournamentsByUserId)
+    this.router.get("/:id", this.getTournamentById)
     this.router.post("/new", this.createTournament)
     this.router.put("/update/:id", this.updateTournament)
   }
@@ -86,6 +87,33 @@ export class TournamentRouter {
       }
       const userId = req.user.sub
       const response = await this.tournamentController.getTournamentsByUserId(userId)
+      if (response.status === "error") {
+        return res.status(response.code).send({
+          status: "error",
+          message: response.message,
+        })
+      }
+      return res.status(response.code).send({
+        status: response.status,
+        data: response.data,
+        message: response.message,
+      })
+    } catch (error: any) {
+      return res.status(500).send({
+        status: "error",
+        message: error.message,
+      })
+    }
+  }
+  private getTournamentById = async (req: Request, res: Response) => {
+    try {
+      if (!req.user) {
+        return res.status(403).send({
+          message: "unauthorized",
+        })
+      }
+      const id = req.params.id
+      const response = await this.tournamentController.getTournamentById(id)
       if (response.status === "error") {
         return res.status(response.code).send({
           status: "error",
