67f44693c4c707291ef30ef4bca770131c5e7f49
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-23 15:51:47 -0500

    feat: Add route to retrieve game by ID

--- src/routes/GameRouter.ts
+++ src/routes/GameRouter.ts
@@ -12,6 +12,7 @@ export class GameRouter {
     this.router = Router()
     this.router.get("/all", this.getAllGames)
     this.router.post("/new", this.createGame)
+    this.router.get("/:id", this.getGameById)
   }
   static getRouter(): Router {
     if (!GameRouter.instance) {
@@ -54,6 +55,33 @@ export class GameRouter {
       }
       const userId = req.user.sub
       const response = await this.gameController.createGame(req.body, userId)
+      if (response.status === "error") {
+        return res.status(response.code).send({
+          status: "error",
+          message: response.message,
+        })
+      }
+      return res.status(response.code).send({
+        status: response.status,
+        data: response.data,
+        message: response.message,
+      })
+    } catch (error: any) {
+      return res.status(500).send({
+        status: "error",
+        message: error.message,
+      })
+    }
+  }
+  private getGameById = async (req: Request, res: Response) => {
+    try {
+      if (!req.user) {
+        return res.status(403).send({
+          message: "unauthorized",
+        })
+      }
+      const id = req.params.id
+      const response = await this.gameController.getGameById(id)
       if (response.status === "error") {
         return res.status(response.code).send({
           status: "error",
