ccd5e51e9c7eda58f805767b5d100b40441c9682
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-05-23 18:41:13 -0500

    feat: Update TournamentRepositorie to include findByIdAndUpdate method

--- src/repositories/TournamentRepositorie.ts
+++ src/repositories/TournamentRepositorie.ts
@@ -1,18 +1,25 @@
 import { Tournament, TournamentModel } from "../models/Tournament"
 
 export interface TournamentRespositorie {
-  createTournament(data: Tournament, userId: string): Promise<Tournament>
+  createTournament(data: Tournament, name: string, userId: string): Promise<Tournament>
   findTournamentById(id: string): Promise<Tournament | null>
+  findByIdAndUpdate(id: string, data: any): Promise<Tournament | null>
 }
 export class TournamentMongoRepositorie implements TournamentRespositorie {
-  async createTournament(data: Tournament, userId: string): Promise<Tournament> {
+  async createTournament(data: Tournament, name: string, userId: string): Promise<Tournament> {
     const newTournament = new TournamentModel({
       ...data,
+      name,
       user: userId,
     })
     return await newTournament.save()
   }
   async findTournamentById(id: string): Promise<Tournament | null> {
     return await TournamentModel.findById(id)
+  }
+  async findByIdAndUpdate(id: string, data: Tournament): Promise<Tournament | null> {
+    return await TournamentModel.findByIdAndUpdate(id, data, {
+      new: true,
+    })
   }
 }
